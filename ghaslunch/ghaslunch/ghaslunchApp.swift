4c9ca57e9eb79178d435702129ae010e478b6e14
luka <kjh08116@gmail.com>
2026-05-15 18:06:44 +0900

    Add Firebase iOS configuration

--- ghaslunch/ghaslunch/ghaslunchApp.swift
+++ ghaslunch/ghaslunch/ghaslunchApp.swift
@@ -5,11 +5,18 @@
 //  Created by luka on 5/15/26.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ghaslunchApp: App {
     @AppStorage("theme") private var themePreference = ""
+
+    init() {
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
